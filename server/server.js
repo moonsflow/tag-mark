7f6693cb41322416f5034ebd76bdfdbd4eeda9fe
Daniel <dmoon@gcfund.org>
2016-09-21 23:29:51 +0900

    added initial html on server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import Express from 'express';
+import compression from 'compression';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import path from 'path';
@@ -32,11 +33,58 @@ mongoose.connect(serverConfig.mongoURL, (err) => {
   }
 });
 
+app.use(compression());
 app.use(bodyParser.json({ limit: '20mb' }));
 app.use(bodyParser.urlencoded({ limit: '20mb', extended: false }));
-app.use(Express.static(path.resolve(__dirname, '../client')));
+app.use(Express.static(path.resolve(__dirname, '../dist')));
 app.use('/api', bookmarks);
 app.use('/api', tags);
+
+// Render Initial HTML
+const renderFullPage = (html) => {
+
+  // Import Manifests
+  const assetsManifest = process.env.webpackAssets && JSON.parse(process.env.webpackAssets);
+  const chunkManifest = process.env.webpackChunkAssets && JSON.parse(process.env.webpackChunkAssets);
+
+  return `
+    <!doctype html>
+    <html>
+      <head>  
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        ${process.env.NODE_ENV === 'production' ? `<link rel='stylesheet' href='${assetsManifest['/app.css']}' />` : ''}
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">                               
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+      </head>
+      <body>
+        <div id="root">${html}</div>
+        <script>          
+          ${process.env.NODE_ENV === 'production' ?
+    `//<![CDATA[
+          window.webpackManifest = ${JSON.stringify(chunkManifest)};
+          //]]>` : ''}
+        </script>
+        <script src='${process.env.NODE_ENV === 'production' ? assetsManifest['/vendor.js'] : '/vendor.js'}'></script>
+        <script src='${process.env.NODE_ENV === 'production' ? assetsManifest['/app.js'] : '/app.js'}'></script>
+      </body>
+    </html>
+  `;
+};
+
+const renderError = err => {
+  const softTab = '&#32;&#32;&#32;&#32;';
+  const errTrace = process.env.NODE_ENV !== 'production' ?
+    `:<br><br><pre style="color:red">${softTab}${err.stack.replace(/\n/g, `<br>${softTab}`)}</pre>` : '';
+  return renderFullPage(`Server Error${errTrace}`, {});
+};
+
+app.use((req, res) => {
+  res
+    .set('Content-Type', 'text/html')
+    .status(200)
+    .end(renderFullPage(''));
+});
 
 
 // Start app
