7313590ef8a5effcc93e6e5c9dd195d684d5e8bf
Daniel <dmoon@gcfund.org>
2016-11-24 00:23:57 +0900

    create random util functions module

---
+++ client/js/utils/random.js
@@ -0,0 +1,15 @@
+export function integer(min, max) {
+  const minValue = (typeof max !== 'undefined') ? parseInt(min, 10) : 0;
+  const maxValue = (typeof max !== 'undefined') ? parseInt(max, 10) : parseInt(min, 10);
+  return parseInt((Math.random() * (maxValue - minValue)) + minValue, 10);
+}
+
+export function float(min, max) {
+  const minValue = (typeof max !== 'undefined') ? parseFloat(min, 10) : 0;
+  const maxValue = (typeof max !== 'undefined') ? parseFloat(max, 10) : parseFloat(min, 10);
+  return Math.random() * (maxValue - minValue) + minValue;
+}
+
+export function bool(likelihood = 0.5) {
+  return !!Math.random() < likelihood
+}
